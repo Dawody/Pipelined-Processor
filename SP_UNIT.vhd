4802ab630df4b6de4172d04ab682b782a2229b0c
Dawod <mhm.dawod1@gmail.com>
2018-05-05 18:24:41 +0200

    revised and decorated. some modifications are nneded to be added after discussion

--- SP_UNIT.vhd
+++ SP_UNIT.vhd
@@ -2,17 +2,18 @@ Library IEEE;
 use ieee.std_logic_1164.all;
 
 Entity MY_SP_UNIT is
-port (OP_MEM: IN std_logic_vector(4 downto 0);
-      FLUSH2:out std_logic);
+port (
+	OP_MEM	: IN std_logic_vector(4 downto 0);
+      	FLUSH2	:out std_logic);
 end  MY_SP_UNIT;
 
-architecture Arch_SP_UNIT of MY_SP_UNIT is
+architecture SP_UNIT_ARCH of MY_SP_UNIT is
 begin
-FLUSH2 <= '1' when OP_MEM="00101" 
-else '1' when OP_MEM="00011"
-else '1' when OP_MEM="11110" 
-else '1' when OP_MEM="11111"
-else '1' when OP_MEM="00100"
+FLUSH2 <= '1' when OP_MEM="00101"	--CALL --delete here after discussion
+else '1' when OP_MEM="00011"	--RET
+else '1' when OP_MEM="11110" 	--RESET	--maybe deleted or not
+else '1' when OP_MEM="11111"	--INT	--maybe deleted or not
+else '1' when OP_MEM="00100"	--RTI
 else '0' ;
-end Arch_SP_UNIT;
+end SP_UNIT_ARCH;
 

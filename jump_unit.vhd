d7f5e6467f720c66ec427861ce7ff96dd0699c23
Dawod <mhm.dawod1@gmail.com>
2018-05-05 13:47:28 +0200

    jump unit update by omar : adding call to flush the processor

--- jump_unit.vhd
+++ jump_unit.vhd
@@ -2,15 +2,15 @@ LIBRARY IEEE;
 USE IEEE.STD_LOGIC_1164.ALL;
 USE IEEE.numeric_std.all;
 
-ENTITY forwarding_unit IS
+ENTITY JUMP_UNIT IS
 	PORT(
 		op_alu : IN std_logic_vector(4 DOWNTO 0);
 		tflags : IN std_logic_vector(3 DOWNTO 0);
 		FLU1 : OUT std_logic);
-END ENTITY forwarding_unit;
+END ENTITY JUMP_UNIT;
 
 
-ARCHITECTURE arch1 OF forwarding_unit IS
+ARCHITECTURE JUMP_UNIT_ARCH OF JUMP_UNIT IS
 	BEGIN
 	Process (ALL)
 	BEGIN
@@ -22,10 +22,13 @@ ARCHITECTURE arch1 OF forwarding_unit IS
 			FLU1 <= '1';
 		elsif op_alu = "01001" and tflags(2) = '1' then
 			FLU1 <= '1';
+		elsif op_alu = "00101" then
+			FLU1 <= '1';
 		else
 			FLU1 <= '0';
 		end if;
 	end process;
-END arch1;
+END JUMP_UNIT_ARCH;
+
 
 

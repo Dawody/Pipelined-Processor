70a3c57cce97720a7176226104212011fbd4a18c
Dawod <mhm.dawod1@gmail.com>
2018-05-05 13:48:31 +0200

    WB stage is decorated by dawod to make it easier for reading

--- WB_stage.vhd
+++ WB_stage.vhd
@@ -3,41 +3,46 @@ USE IEEE.STD_LOGIC_1164.ALL;
 USE IEEE.numeric_std.all;
 
 ENTITY WB_stage IS
-	PORT(ALUO1 :in std_logic_vector(15 downto 0);
-		B:in std_logic_vector(15 downto 0);
-		MEMO:in std_logic_vector(15 downto 0);
-		ALUO2:in std_logic_vector(15 downto 0);
-		OP_WB:in std_logic_vector(4 downto 0);
-		DATA1:out std_logic_vector(15 downto 0);
-		DATA2 :out std_logic_vector(15 downto 0));
+	PORT(
+		ALUO1 	: in std_logic_vector(15 downto 0);
+		B	: in std_logic_vector(15 downto 0);
+		MEMO	: in std_logic_vector(15 downto 0);
+		ALUO2	: in std_logic_vector(15 downto 0);
+	
+		OP_WB	: in std_logic_vector(4 downto 0);
+		DATA1	:out std_logic_vector(15 downto 0);
+		DATA2 	:out std_logic_vector(15 downto 0));
 END ENTITY WB_stage;
+
 
 ARCHITECTURE Arch_WB_stage OF WB_stage IS
 
+------------------------------TRI STATE------------------------------
 component tri_state is
 port (inp :in std_logic_vector(15 downto 0);
       en :in std_logic;
       oup: out std_logic_vector(15 downto 0));
 end component;
 
-
+-------------------------------MUX3---------------------------------
 component mux3_WB is
 port(ALUO1,B,MEMO:in std_logic_vector(15 downto 0);
 OP:in std_logic_vector(4 downto 0);
 DATA1:out std_logic_vector(15 downto 0));
 end component;
 
+--------------------------------MUX2--------------------------------
 component mux2_WB is
 port(ALUO2,MEMO:in std_logic_vector(15 downto 0);
 OP:in std_logic_vector(4 downto 0);
 DATA2:out std_logic_vector(15 downto 0));
 end component;
 
-
+----------------------------SIGNALS AREA-----------------------
 signal tr0_en,tr1_en,tr2_en,tr3_en,tr4_en:std_logic;
 signal tr0_out,tr1_out,tr2_out,tr3_out,tr4_out:std_logic_vector(15 downto 0);
 
-
+-----------------------------------------------------------------------
 begin
 
 tr1_en <='1' when  OP_WB(4 DOWNTO 0)="11010"

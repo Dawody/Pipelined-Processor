0781011c58f3106d33426a3d73e69536037460bd
Dawod <mhm.dawod1@gmail.com>
2018-05-06 12:42:36 +0200

    SP unit don't flush in call operation

--- SP_UNIT.vhd
+++ SP_UNIT.vhd
@@ -9,7 +9,7 @@ end  MY_SP_UNIT;
 
 architecture SP_UNIT_ARCH of MY_SP_UNIT is
 begin
-FLUSH2 <= '1' when OP_MEM="00101"	--CALL --delete here after discussion
+--FLUSH2 <= '1' when OP_MEM="00101"	--CALL --delete here after discussion
 else '1' when OP_MEM="00011"	--RET
 else '1' when OP_MEM="11110" 	--RESET	--maybe deleted or not
 else '1' when OP_MEM="11111"	--INT	--maybe deleted or not
